c51ed84bdb473ad7c7d1231dcae57a946c704696
Dave_12138 <207955131@qq.com>
2026-03-05 18:58:58 +0800

    test

--- wiki-ipe-config.js
+++ wiki-ipe-config.js
@@ -1,13 +1,16 @@
 // InPageEdit NEXT
-document.body.append(
-    Object.assign(document.createElement('script'), {
-        src: 'https://cdn.jsdelivr.net/npm/@inpageedit/core/dist/index.js',
-        type: 'module',
-    })
-)
+// const IPE_LINK = "https://cdn.jsdelivr.net/npm/@inpageedit/core/dist/index.js"
+// document.body.append(
+//     Object.assign(document.createElement('script'), {
+//         src: IPE_LINK,
+//         type: 'module',
+//     })
+// )
+import { Schema } from "https://cdn.jsdelivr.net/npm/@inpageedit/core/dist/index.js";
 mw.hook('InPageEdit.ready').add(function (ipe) {
     ipe.plugin({
         inject: ['preferences'],
+        name: "sync-preferences",
         apply: function (ctx) {
             ctx.preferences.setMany({
                 "analytics.enabled": true,
@@ -25,6 +28,33 @@ mw.hook('InPageEdit.ready').add(function (ipe) {
                 "quickRedirect.reason": "[IPEN:Redirect] ",
                 "quickUpload.summary": "[IPEN:Upload] ",
                 "toolboxAlwaysShow": true
+            })
+        },
+    })
+    ipe.plugin({
+        inject: ['preferences'/* ,'inArticleLinks' */],
+        name: "format-edit-summary",
+        PreferencesSchema: Schema.object({
+            "formatEditSummary.template": Schema.string().description('编辑摘要模板'),
+        }),
+        PreferencesDefaults: {
+            "formatEditSummary.template": '[IPEN] /* ${section} */ ',
+        },
+        apply: function (ctx) {
+            ctx.preferences.defineCategory({
+                name: "format-edit-summary",
+                index: 15
+            })
+            ctx.on('in-article-links/anchor-parsed', async (paylo) => {
+                /**@type {HTMLAnchorElement} */
+                const a = paylo.anchor;
+                const sectionName = a.title?.replace(/^.*?：/, '');
+                console.log("章节：", sectionName, a);
+                setTimeout(() => {
+                    const qeb = a.querySelector('+a.ipe-quick-edit');
+                    console.log(qeb);
+
+                }, 10);
             })
         },
     })
